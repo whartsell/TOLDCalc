67de8b89b86a4c7da500b76bd389d8f1ff71ba00
Will Hartsell <whartsell@homeaway.com>
2015-01-12 22:05:08 -0600

    refusalspeed work

---
+++ src/main/java/net/willshouse/planner/A10C.java
@@ -0,0 +1,8 @@
+package net.willshouse.planner;
+
+/**
+ * Created by whartsell on 1/12/15.
+ */
+public class A10C {
+    private double emptyWeight;
+}
---
+++ src/main/java/net/willshouse/planner/RefusalSpeed.java
@@ -0,0 +1,36 @@
+package net.willshouse.planner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by whartsell on 1/12/15.
+ */
+public class RefusalSpeed extends PerformanceChart {
+    Map<Double, ChartSeries> step1, step2, step3;
+
+    public RefusalSpeed() throws IOException {
+        super("RefusalSpeed.json");
+        step1 = toChartSeriesMap("Step1");
+        step2 = toChartSeriesMap("Step2");
+        step3 = toChartSeriesMap("Step3");
+
+    }
+
+    public Map<String, Double> calculate(double pressureAlt, double temperature, double runwayLength, double takeOffIndex, double grossWeight, double wind, double RCR, boolean speedBrakesClosed) throws IOException {
+
+        double limitSpeed = new WheelBrakeEnergyLimitSpeed().calculate(pressureAlt, temperature,
+                wind, grossWeight, speedBrakesClosed).get("WheelBrake Energy Limit Speed");
+
+        Map<String, Double> results = new HashMap<String, Double>();
+        double step1FofX = interpolateBetweenSeries(step1, takeOffIndex, runwayLength);
+        double step2FofX = interpolateBetweenSeries(step2, step1FofX, grossWeight);
+        double refusalSpeed = interpolateBetweenSeries(step3, step2FofX, RCR) + wind;
+        if (limitSpeed < refusalSpeed) refusalSpeed = limitSpeed;
+        results.put("refusal speed", refusalSpeed);
+
+
+        return results;
+    }
+}
