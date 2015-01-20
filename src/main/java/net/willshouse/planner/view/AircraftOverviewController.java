c3463310b43bf38849bf7ced52e85bf4a2ae0990
Will Hartsell <whartsell@homeaway.com>
2015-01-20 15:17:50 -0600

    first bits of test gui

---
+++ src/main/java/net/willshouse/planner/view/AircraftOverviewController.java
@@ -0,0 +1,30 @@
+package net.willshouse.planner.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by whartsell on 1/20/15.
+ */
+public class AircraftOverviewController {
+    @FXML
+    private TextField grossWeightField;
+
+    @FXML
+    private TextField dragIndexField;
+
+    @FXML
+    private ChoiceBox<String> speedBrakesChoiceBox;
+
+    @FXML
+    private ChoiceBox<Integer> flapsChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> thrustChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> antiSkidChoiceBox;
+
+
+}
